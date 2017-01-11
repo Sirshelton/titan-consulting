f229e39fee0b1929b94a78ad3ae1c4832c1f0a87
Semaj Anderson <sanderson@icstars.org>
2017-01-11 18:58:45 +0000

    semaj added content

--- backend/routes.js
+++ backend/routes.js
@@ -17,39 +17,21 @@ router.get('/omar', function(req,res){
   router.get('/trains', function(req,res){
     return res.render('trains.html');
   });
-<<<<<<< HEAD
   
   
   router.get('/semaj', function(req,res){
     return res.render('semaj.html');
   });
   
-=======
-   
-   router.get('/charlie', function(req,res){
-    return res.render('charlie.html');
-  });
-  
-  router.get('/C2.html', function(req,res){
-    return res.render('C2.html');
-  });
->>>>>>> 4b4d0fb9a95c9b2914f1c38f441b88d2eb766e44
 
   /*router.get('/trains/data', function(req,res){
     database.executeQuery("SELECT * FROM trains", function(results) {
       res.send(results);
     });
-<<<<<<< HEAD
   });*/
-<<<<<<< HEAD
 
     /* Your code here */
       return router
-=======
->>>>>>> 1842506b5d6c450766e01eca9a53eea62e3c5b69
-=======
-  });                                                                           
->>>>>>> 4b4d0fb9a95c9b2914f1c38f441b88d2eb766e44
 
   /* Your code here */
   router.get('/loginpage', function(req,res){
