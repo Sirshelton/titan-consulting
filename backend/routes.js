290f677df9925b5f33ce08e610fd3db9d648d8e2
omar <pcardenas@icstars.org>
2017-01-11 16:10:15 +0000

    Omar added files

--- backend/routes.js
+++ backend/routes.js
@@ -9,15 +9,20 @@ module.exports = function() {
     return res.render('helloworld.html');
   });
 
+router.get('/omar', function(req,res){
+    return res.render('omar.html');
+  });
+
+
   router.get('/trains', function(req,res){
     return res.render('trains.html');
   });
 
-  router.get('/trains/data', function(req,res){
+  /*router.get('/trains/data', function(req,res){
     database.executeQuery("SELECT * FROM trains", function(results) {
       res.send(results);
     });
-  });
+  });*/
 
   /* Your code here */
 
