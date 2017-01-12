67f6b6ddca60802545cfb8f8e661b66cf639092a
Charlie <cderbigny@icstars.org>
2017-01-12 23:30:50 +0000

    Charlie update changes

--- backend/routes.js
+++ backend/routes.js
@@ -22,29 +22,31 @@ router.get('/omar', function(req,res){
     return res.render('challengewall.html');
   });
   
+  router.get('/c2.html', function(req,res){
+    return res.render('C2.html');
+  });
+  
+  
   router.get('/semaj', function(req,res){
     return res.render('semaj.html');
   });
   
-
-  /*router.get('/trains/data', function(req,res){
-    database.executeQuery("SELECT * FROM trains", function(results) {
-      res.send(results);
-    });
-<<<<<<< HEAD
-  });*/
-
-    /* Your code here */
-      return router
-=======
-  });                                                                           
->>>>>>> 4b4d0fb9a95c9b2914f1c38f441b88d2eb766e44
-
-  /* Your code here */
   router.get('/loginpage', function(req,res){
     return res.render('loginpage.html');
   });
   
-  return router
->>>>>>> 4b4d0fb9a95c9b2914f1c38f441b88d2eb766e44
-}();
+  /*
+
+  router.get('/trains/data', function(req,res){
+    database.executeQuery("SELECT * FROM trains", function(results) {
+      res.send(results);
+    });
+
+  });*/
+
+    /* Your code here*/ 
+      return router
+
+  }();
+    
+
